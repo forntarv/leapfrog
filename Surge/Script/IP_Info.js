0e45c0ffc3f3cac8d8b4f0de5702782bca41b55d
For(n)tarv <110398313+forntarv@users.noreply.github.com>
2022-11-12 00:15:12 +0800

    Create IP_Info.sgmodule

---
+++ Surge/Script/IP_Info.js
@@ -0,0 +1,48 @@
+;(async () => {
+
+    let params = getParams($argument)
+    let proxy = await httpAPI("/v1/policy_groups");
+    let allGroup = [];
+    
+    for (var key in proxy){
+        allGroup.push(key)
+    }
+    
+    let group = params.group
+    let rootName = (await httpAPI("/v1/policy_groups/select?group_name="+encodeURIComponent(group)+"")).policy;
+    
+    while(allGroup.includes(rootName)==true){
+        rootName = (await httpAPI("/v1/policy_groups/select?group_name="+encodeURIComponent(rootName)+"")).policy;
+    }
+    
+    $httpClient.get('http://ip-api.com/json/?lang=en', function (error, response, data) {
+        const jsonData = JSON.parse(data);
+        $done({
+            title: rootName,
+            content:
+                '${jsonData.country} - ${jsonData.city}\n' +
+                '${jsonData.as}\n' +
+                'IP: ${jsonData.query}',
+            icon: params.icon,
+            "icon-color": params.color
+        });
+    });
+    
+})();    
+
+function httpAPI(path = "", method = "GET", body = null) {
+    return new Promise((resolve) => {
+        $httpAPI(method, path, body, (result) => {
+            resolve(result);
+        });
+    });
+};
+    
+function getParams(param) {
+    return Object.fromEntries(
+        $argument
+            .split("&")
+            .map((item) => item.split("="))
+            .map(([k, v]) => [k, decodeURIComponent(v)])
+    );
+}
